0c342013e2d008baf9b8fbb51b1d2e59ad0452c2
Rodrigo Coelho <rodrigopcoelho2000@gmail.com>
2023-08-15 14:49:27 +0100

    feito

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,16 @@
 Rails.application.routes.draw do
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
-  # Defines the root path route ("/")
-  # root "articles#index"
+  # verb "path", to: "controller#action"
+  # CRUD (create, read, update, destroy)
+  # See all tasks
+  #get 'tasks', to: 'tasks#index'
+  # Create a task
+  #get 'tasks/new', to: 'tasks#new'
+  #post 'tasks', to: 'tasks#create' # submit the form
+  #get 'tasks/:id', to: 'tasks#show', as: :task
+  # update
+  #get 'tasks/:id/edit', to: 'tasks#edit', as: :edit_task
+  #patch 'tasks/:id', to: 'tasks#update' # submit the form
+  # # Destroy a restaurant
+  #delete "tasks/:id", to: "tasks#destroy" , as: :delete_task
+  resources :tasks
 end
